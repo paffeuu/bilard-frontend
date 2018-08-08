07272445c7d5b972befea4bd3709d24cb78a7533
Dzimid <jac3k.dziemidowicz@gmail.com>
2018-08-08 12:55:15 +0200

    Komunikacja miedzy modułami poprzez service

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -9,38 +9,47 @@ import {DataService} from "../../shared/service/data.service";
 })
 export class TableComponent implements OnInit {
   image: any;
+  divisionLines: number;
+  canvas: any;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService) {
+  }
 
   ngOnInit() {
+    this.dataService
+      .getDivision()
+      .subscribe(response => {
+        this.divisionLines = parseInt(response.division);
+        this.drawDivisionLines(this.divisionLines);
+      }, error => {
+        console.error(error);
+      });
   }
 
   @ViewChild('poolTableView') poolTableView: ElementRef;
   public c: CanvasRenderingContext2D;
 
   ngAfterViewInit(): void {
-    this.c = (<HTMLCanvasElement>this.poolTableView.nativeElement).getContext('2d');
+    this.canvas = (<HTMLCanvasElement>this.poolTableView.nativeElement);
+    this.c = this.canvas.getContext('2d');
     this.image = new Image();
     this.image.src = 'https://preview.ibb.co/mAZX1K/table_cut01.jpg';
 
-    var that = this;
-    this.image.onload = function()  {
+    let that = this;
+    this.image.onload = function () {
       that.drawPoolTableImage(that.image);
     }
   }
 
-  drawDivisionLines(): void {
+  drawDivisionLines(lines: number): void {
     this.c.beginPath();
     this.c.clearRect(0, 0, 1000, 500);
     this.drawPoolTableImage(this.image);
 
-    var width = 1000;
-    var height = 500;
-
-    for (var i = 1; i <= this.dataService.division; ++i) {
-      var parts = parseInt(String(this.dataService.division)) + 1;
-      this.c.moveTo(width / parts * i, 0);
-      this.c.lineTo(width / parts * i, height);
+    for (let i = 1; i <= lines; ++i) {
+      let parts = lines + 1;
+      this.c.moveTo(this.canvas.width / parts * i, 0);
+      this.c.lineTo(this.canvas.width / parts * i, this.canvas.height);
     }
 
     this.c.stroke();
