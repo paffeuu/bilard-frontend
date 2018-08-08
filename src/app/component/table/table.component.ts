20da567d3e84872c280ed36238365a6b0e6f8d7b
Dzimid <jac3k.dziemidowicz@gmail.com>
2018-08-08 10:55:44 +0200

    Wczytanie obrazu do canvas, narysowanie testowej lini

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -12,4 +13,21 @@ export class TableComponent implements OnInit {
   ngOnInit() {
   }
 
+  @ViewChild('poolTableView') poolTableView: ElementRef;
+  public c: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+    this.c = (<HTMLCanvasElement>this.poolTableView.nativeElement).getContext('2d');
+    var image = new Image();
+    image.src = 'https://preview.ibb.co/mAZX1K/table_cut01.jpg';
+
+    var that = this;
+    image.onload = function () {
+      that.c.drawImage(image, 0, 0, 1000, 500);
+      that.c.beginPath();
+      that.c.moveTo(0, 0);
+      that.c.lineTo(100, 100);
+      that.c.stroke();
+    }
+  }
 }
