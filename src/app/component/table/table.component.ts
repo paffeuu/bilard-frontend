d54fd1dfce01ca2a291156d7407d814ffef5c9e8
Dzimid <jac3k.dziemidowicz@gmail.com>
2018-08-09 08:39:09 +0200

    Rozwiazanie problemu z mrugającymi liniami

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -11,11 +11,13 @@ export class TableComponent implements OnInit {
   image: any;
   divisionLines: number;
   canvas: any;
+  endPoint = 'http://localhost:8090/pooltable/get-snapshot';
+  fps = 4; // Klatki na sekunde
 
   constructor(private dataService: DataService) {
     setInterval(() => {
       this.refreshComponent();
-    }, 500);
+    }, 1000 / this.fps);
   }
 
   ngOnInit() {
@@ -35,17 +37,20 @@ export class TableComponent implements OnInit {
     this.canvas = (<HTMLCanvasElement>this.poolTableView.nativeElement);
     this.c = this.canvas.getContext('2d');
     this.image = new Image();
-    this.image.src = 'http://localhost:8090/pooltable/get-snapshot';
+    this.image.src = this.endPoint;
 
     let that = this;
     this.image.onload = function () {
       that.drawPoolTableImage(that.image);
+      that.drawDivisionLines();
     }
   }
 
-  drawDivisionLines(lines: number): void {
-    for (let i = 1; i <= lines; ++i) {
-      let parts = lines + 1;
+  drawDivisionLines(): void {
+    this.c.beginPath();
+
+    for (let i = 1; i <= this.divisionLines; ++i) {
+      let parts = this.divisionLines + 1;
       this.c.moveTo(this.canvas.width / parts * i, 0);
       this.c.lineTo(this.canvas.width / parts * i, this.canvas.height);
     }
@@ -53,16 +58,11 @@ export class TableComponent implements OnInit {
     this.c.stroke();
   }
 
-  drawPoolTableImage(image): void {
+  drawPoolTableImage(image: any): void {
     this.c.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
-    this.drawDivisionLines(this.divisionLines);
   }
 
   refreshComponent(): void {
-    this.image.src = 'http://localhost:8090/pooltable/get-snapshot?' + new Date().getTime();
-
-    this.c.beginPath();
-    this.c.clearRect(0, 0, 1000, 500);
-    this.drawPoolTableImage(this.image);
+    this.image.src = this.endPoint + '?' + new Date().getTime();
   }
 }
