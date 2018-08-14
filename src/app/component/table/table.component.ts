744829c43971d964e10a3e3958b130e927ce3403
Paweł Kowański <pawel.kowanski@hotmail.com>
2018-08-14 08:08:44 +0200

    *highlighted on mouse event cue balls, divison lines and table image are correctly shown on the screen with PaperJS

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
 import {ViewChild} from '@angular/core';
 import {DataService} from "../../shared/service/data.service";
-import {PaperScope, Path, Point, Project, Raster, Group, Item, View, Size} from 'paper';
+import {PaperScope, Path, Point, Project, Raster, Group, Item} from 'paper';
 import {environment} from "../../../environments/environment";
 import {PoolTableService} from "../../shared/service/pool.table.service";
 import {PoolTableModel} from "../../shared/model/pool.table.model";
@@ -11,27 +11,6 @@ const tableWidth = 2048;  // rozdzielczość zdjęcia to 2048 x 1536
 const tableHeight = 1536;
 const tableScale = 0.5;   // skala -> image/source-image
 const radius = 10;
-const cueBallsDataModel = [
-  {i: 0, x: 1122.5, y: 1085.5},
-  {i: 1, x: 1088.5, y: 713.5},
-  {i: 2, x: 880.5, y: 503.5},
-  {i: 3, x: 1182.5, y: 438.5},
-  {i: 4, x: 1910.5, y: 1159.5},
-  {i: 5, x: 1358.5, y: 742.5},
-  {i: 6, x: 1323.5, y: 483.5},
-  {i: 7, x: 1571.5, y: 998.5},
-  {i: 8, x: 1420.5, y: 714.5},
-  {i: 9, x: 1603.5, y: 1029.5},
-  {i: 10, x: 1537.5, y: 985.5},
-  {i: 11, x: 1896.5, y: 394.5},
-  {i: 12, x: 1623.5, y: 536.5},
-  {i: 13, x: 1196.5, y: 1009.5},
-  {i: 14, x: 1758.5, y: 613.5},
-  {i: 15, x: 1749.5, y: 895.5},
-  {i: 16, x: 1754.5, y: 857.5},
-  {i: 17, x: 144.5, y: 112.5},
-  {i: 18, x: 259.5, y: 107.5}
-];  // przykładowy model danych
 
 @Component({
   selector: 'app-table',
@@ -69,9 +48,7 @@ export class TableComponent implements OnInit {
     this.project = new Project(this.poolTableView.nativeElement)
 
     this.initalizeCanvas();
-
-    this.drawDivisionLines();
-    this.drawCueBalls();
+    this.refreshComponent();
   }
 
   initalizeCanvas(): void {
@@ -122,7 +99,6 @@ export class TableComponent implements OnInit {
   }
 
   drawCueBalls(): void {
-    // TO-DO kulki się rozjeżdżają dokładnie jak 'beczkowaty' obraz z kamery - problem z image distortion
     let solids = this.project.activeLayer.children["solids"];     // bile "całe"
     solids.removeChildren();
     let stripes = this.project.activeLayer.children["stripes"];    // bile "połówki"
@@ -133,7 +109,7 @@ export class TableComponent implements OnInit {
       let circle = new Path.Circle(new Point(ball.x * tableScale, ball.y * tableScale), radius);
       if (ball.id % 2 == 0) { // tu warunek dla całych i połówek <-- random
         solids.addChild(circle);
-        circle.onMouseMove = function() {    // tu będzie podświetlanie całych lub połówek
+        circle.onMouseMove = function() {    // tu będzie podświetlanie całych
           let solids = this.project.activeLayer.children["solids"];
           solids.children.forEach((circle) => that.setCircleHighlighted(circle, "red"));
         };
@@ -142,7 +118,7 @@ export class TableComponent implements OnInit {
         }
       } else {
         stripes.addChild(circle);
-        circle.onMouseMove = function () {    // tu będzie podświetlanie całych lub połówek
+        circle.onMouseMove = function () {    // tu będzie podświetlanie połówek
           stripes.children.forEach((circle) => that.setCircleHighlighted(circle, "white"));
         }
         circle.onMouseLeave = function () {
@@ -172,7 +148,7 @@ export class TableComponent implements OnInit {
 
   setCircleUnhighlighted(circle: Item) {
     circle.fillColor = "black";
-    circle.opacity = 1;     // im bliżej 0, tym bardziej nie widać bil
+    circle.opacity = 0.1;     // im bliżej 0, tym bardziej nie widać bil
   }
 
 }
